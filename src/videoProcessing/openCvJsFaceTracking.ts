263bbb6cbdb235784741de857c74bfc79d35781a
Akosah <emmanuel.s.akosah.19@gmail.com>
2022-09-27 13:50:14 -0400

    wip opencv installation

--- src/videoProcessing/openCvJsFaceTracking.ts
+++ src/videoProcessing/openCvJsFaceTracking.ts
@@ -1,5 +1,17 @@
 /*
  processes video stream and provides mouse commands
  */
+const cv = require("@u4/opencv4nodejs");
 
-function processVideo() {}
+function processVideo() {
+  const rows = 100; // height
+  const cols = 100; // width
+
+  // empty Mat
+  const emptyMat = new cv.Mat(rows, cols, cv.CV_8UC3);
+
+  // fill the Mat with default value
+  const whiteMat = new cv.Mat(rows, cols, cv.CV_8UC1, 255);
+  const blueMat = new cv.Mat(rows, cols, cv.CV_8UC3, [255, 0, 0]);
+}
+export { processVideo };
