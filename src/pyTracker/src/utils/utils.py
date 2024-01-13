ae0173f73e53ac71d57cfa4752f824b6b2f6f357
Farid Karimli <karimli.farid@icloud.com>
2024-01-13 20:49:53 +0400

    writing face tracking info to file

--- src/pyTracker/src/utils/utils.py
+++ src/pyTracker/src/utils/utils.py
@@ -28,3 +28,18 @@ def getBoundingCoordinatesOfScalingBox():
     endY = 0.5 +  yBand
     boundingBox = {"x":[startX,endX] , "y":[startY,endY] }
     return boundingBox
+
+"""
+writes face tracking log to a file
+"""
+def writeFaceTrackingLogToFile(face, frameSize, pose, pos, guesture, face_confidence, gesture_confidences):
+    f = open("trackingLog.txt", "a")
+    f.write("face: " + str(face) + "\n")
+    f.write("frameSize: " + str(frameSize) + "\n")
+    f.write("pose: " + str(pose) + "\n")
+    f.write("pos: " + str(pos) + "\n")
+    f.write("guesture: " + str(guesture) + "\n")
+    f.write("face_confidence: " + str(face_confidence) + "\n")
+    f.write("gesture_confidences: " + str(gesture_confidences) + "\n")
+    f.write("--")
+    f.close()
